9f7a667bd11f769200e46b9e1da5fb578fb26e14
Takumi Ishihara <50577904+takuan517@users.noreply.github.com>
2020-03-16 09:21:02 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "0eUojPhrkP9xQtJac7c3CTp1E"
-  config.consumer_secret     = "7GCAWQZ2kJ1FcOKZql1v1Qgs9LhI17Ksgbf7X0QY2ShVmYE0KR"
-  config.access_token        = "708208212026085377-qEonm7He7N4QuBYXGpbznkvPajpUyyj"
-  config.access_token_secret = "sZsiZWFvtmC6R6YuVecV2kuODvb7Co8Dk9Ww8wAi2CV1n"
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
 end
 
 stream_client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = "0eUojPhrkP9xQtJac7c3CTp1E"
-  config.consumer_secret     = "7GCAWQZ2kJ1FcOKZql1v1Qgs9LhI17Ksgbf7X0QY2ShVmYE0KR"
-  config.access_token        = "708208212026085377-qEonm7He7N4QuBYXGpbznkvPajpUyyj"
-  config.access_token_secret = "sZsiZWFvtmC6R6YuVecV2kuODvb7Co8Dk9Ww8wAi2CV1n"
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
 end
 
 client.update("rubyでtwitter apiいじってみた")
